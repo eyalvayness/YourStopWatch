7d05a7fbe349118b25819235b3e5b1a07d3d18de
Eyal Vayness <evayness@gmail.com>
2019-01-17 17:01:51 +0100

    adding the direct add button

--- YourStopWatch/MainActivity.cs
+++ YourStopWatch/MainActivity.cs
@@ -22,18 +22,18 @@ namespace YourStopWatch
     {
         LinearLayout outputContainer;
         BottomNavigationView navigation;
-        ImageView imgView;
+        ImageView stopwatchImgView;
         Bitmap bmp;
         Canvas canv;
-        RelativeLayout container;
+        RelativeLayout container, stopwatchLayout, listLayout, graphicsLayout, settingsLayout;
         Paint contour, background, secPaint, minPaint, hourPaint;
         DateTime absoluteRef;
         Timer timer;
         View addedView = null;
         TextView timerView;
-        Button startButton, stopButton, pauseButton, resetButton;
         readonly string dbFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
         const string dbName = "SavedTimesDataBase.db3", settingsName = "AppSettings";
+        bool showCircle;
         int milli = 0, sec = 0, min = 0, hour = 0, maxHour = 6, bitmapLength = 500;
         const int maxSec = 60, maxMin = 60;
         float secOffset, minOffset, hourOffset;
@@ -49,9 +49,15 @@ namespace YourStopWatch
             LayoutTransition trans = new LayoutTransition();
             container.LayoutTransition = trans;
             addedView = null;
-            StopWatchPageOutput();
+            
+            stopwatchLayout = StopWatchLayoutSetup();
+            listLayout = ListLayoutSetup();
+            graphicsLayout = GraphicsLayoutSetup();
+            settingsLayout = SettingsLayoutSetup();
 
-            ToggleButtonsEndTimer();
+            StopWatchLayoutOutput();
+
+            ToggleButtons(ButtonsState.End);
 
             navigation = FindViewById<BottomNavigationView>(Resource.Id.bottomNavigation);
             navigation.SetOnNavigationItemSelectedListener(this);
@@ -123,6 +129,7 @@ namespace YourStopWatch
             sec = settings.GetInt("timerSeconds", 0);
             min = settings.GetInt("timerMinutes", 0);
             hour = settings.GetInt("timerHours", 0);
+            showCircle = settings.GetBoolean("showCircle", true);
 
             secOffset = bitmapLength / 5f;
             minOffset = bitmapLength / 10f;
@@ -145,37 +152,21 @@ namespace YourStopWatch
             else
                 settings.PutBoolean("isTimerRunning", false);
 
+            settings.PutBoolean("showCircle", showCircle);
             settings.Commit();
         }
 
-        private void CommonPageSetup(int pageId)
+        private RelativeLayout StopWatchLayoutSetup()
         {
-            if (addedView != null)
-                container.RemoveView(addedView);
+            RelativeLayout layout = (RelativeLayout)((LayoutInflater)BaseContext.GetSystemService(Context.LayoutInflaterService)).Inflate(Resource.Layout.stopwatch_layout, null);
 
-            LayoutInflater inflater = (LayoutInflater)BaseContext.GetSystemService(Context.LayoutInflaterService);
-            addedView = inflater.Inflate(pageId, null);
-            container.AddView(addedView);
-        }
+            Button startButton = layout.FindViewById<Button>(Resource.Id.startButton);
+            Button stopButton = layout.FindViewById<Button>(Resource.Id.stopButton);
+            Button pauseButton = layout.FindViewById<Button>(Resource.Id.pauseButton);
+            Button resetButton = layout.FindViewById<Button>(Resource.Id.resetButton);
 
-        private void StopWatchPageSetup()
-        {
-
-        }
-
-        private void StopWatchPageOutput()
-        {
-            CommonPageSetup(Resource.Layout.stopwatch_layout);
-            startButton = FindViewById<Button>(Resource.Id.startButton);
-            stopButton = FindViewById<Button>(Resource.Id.stopButton);
-            pauseButton = FindViewById<Button>(Resource.Id.pauseButton);
-            resetButton = FindViewById<Button>(Resource.Id.resetButton);
-
-            timerView = FindViewById<TextView>(Resource.Id.timerView);
-            imgView = FindViewById<ImageView>(Resource.Id.imgView);
-
-            bmp = Bitmap.CreateBitmap(bitmapLength, bitmapLength, Bitmap.Config.Argb8888);
-            canv = new Canvas(bmp);
+            timerView = layout.FindViewById<TextView>(Resource.Id.timerView);
+            stopwatchImgView = layout.FindViewById<ImageView>(Resource.Id.imgView);
 
             contour = new Paint();
             background = new Paint();
@@ -190,55 +181,118 @@ namespace YourStopWatch
             minPaint.Color = Color.MediumBlue;
             hourPaint.Color = Color.OrangeRed;
 
-            canv.DrawBitmap(bmp, 0, 0, background);
-            UpdateClock();
-            SetButtonsListeners();
-            if (timer == null)
-                ToggleButtonsEndTimer();
-            else if (!timer.Enabled)
-            {
-                pauseButton.Text = "continue";
-                ToggleButtonsPauseTimer();
-            }
-            else
-                ToggleButtonsStartTimer();
+            SetButtonsListeners(startButton, stopButton, pauseButton, resetButton);
+
+            return layout;
         }
 
-        private void ListPageOutput()
+        private RelativeLayout ListLayoutSetup()
         {
-            CommonPageSetup(Resource.Layout.list_layout);
-            outputContainer = FindViewById<LinearLayout>(Resource.Id.outputContainer);
-            TextView title = FindViewById<TextView>(Resource.Id.dashboardTitle);
-            title.Gravity = GravityFlags.CenterHorizontal;
+            RelativeLayout layout = (RelativeLayout)((LayoutInflater)BaseContext.GetSystemService(Context.LayoutInflaterService)).Inflate(Resource.Layout.list_layout, null);
+            outputContainer = layout.FindViewById<LinearLayout>(Resource.Id.outputContainer);
 
             var db = new SQLiteConnection(System.IO.Path.Combine(dbFolder, dbName));
             db.CreateTable<Time>();
             var table = db.Table<Time>();
 
-
-            if (table.Count() == 0)
-                Toast.MakeText(this, "There is no registered time for the moment.", ToastLength.Long).Show();
-
             foreach (var t in table)
-            {
-                LayoutInflater inflater = (LayoutInflater)BaseContext.GetSystemService(Context.LayoutInflaterService);
-                View timeView = inflater.Inflate(Resource.Layout.single_output_layout, null);
-                TextView textOutput = timeView.FindViewById<TextView>(Resource.Id.outText);
-                Button removeButton = timeView.FindViewById<Button>(Resource.Id.removeButton);
-                outputContainer.AddView(timeView);
+                AddTimeToOutputList(t, db);
 
-                textOutput.Text = string.Format("{0}\n{1}", t.TimeSaved.ToLongDateString(), t.TimeSaved.ToLongTimeString());
-
-                removeButton.Click += delegate
-                {
-                    outputContainer.RemoveView(timeView);
-                    db.Delete<Time>(t.Id);
-                    Toast.MakeText(this, $"The {t.TimeSaved.ToLongTimeString()} of {t.TimeSaved.ToShortDateString()} has been deleted.", ToastLength.Long).Show();
-                };
-            }
+            return layout;
         }
 
-        private void SetButtonsListeners()
+        private RelativeLayout GraphicsLayoutSetup()
+        {
+            RelativeLayout layout = (RelativeLayout)((LayoutInflater)BaseContext.GetSystemService(Context.LayoutInflaterService)).Inflate(Resource.Layout.graphics_layout, null);
+            TextView title = layout.FindViewById<TextView>(Resource.Id.graphicsTitle);
+
+            return layout;
+        }
+
+        private RelativeLayout SettingsLayoutSetup()
+        {
+            RelativeLayout layout = (RelativeLayout)((LayoutInflater)BaseContext.GetSystemService(Context.LayoutInflaterService)).Inflate(Resource.Layout.settings_layout, null);
+            SeekBar radiusSB = layout.FindViewById<SeekBar>(Resource.Id.radius_seek_bar);
+            TextView radiusSBValue = layout.FindViewById<TextView>(Resource.Id.seekbar_value);
+            ToggleButton toggleBoxShowCircle = layout.FindViewById<ToggleButton>(Resource.Id.show_circle_checkbox);
+            NumberPicker maxHourPicker = layout.FindViewById<NumberPicker>(Resource.Id.max_hour_picker);
+
+            maxHourPicker.MinValue = 1;
+            maxHourPicker.MaxValue = 8;
+            maxHourPicker.Value = maxHour;
+            maxHourPicker.ValueChanged += delegate
+            {
+                maxHour = maxHourPicker.Value;
+            };
+
+            radiusSB.SetProgress(bitmapLength, false);
+            radiusSBValue.Text = radiusSB.Progress.ToString() + "px";
+            radiusSB.ProgressChanged += delegate
+            {
+                radiusSBValue.Text = radiusSB.Progress.ToString() + "px";
+                bitmapLength = radiusSB.Progress;
+
+                secOffset = bitmapLength / 5f;
+                minOffset = bitmapLength / 10f;
+            };
+
+            toggleBoxShowCircle.TextOff = "Off";
+            toggleBoxShowCircle.TextOn = "On";
+            toggleBoxShowCircle.Checked = showCircle;
+            toggleBoxShowCircle.CheckedChange += delegate
+            {
+                showCircle = toggleBoxShowCircle.Checked;
+            };
+
+            return layout;
+        }
+
+        private void CommonPageOutput(RelativeLayout layout)
+        {
+            if (addedView != null)
+                container.RemoveView(addedView);
+            container.AddView(layout);
+            addedView = layout;
+        }
+
+        private void StopWatchLayoutOutput()
+        {
+            bmp = Bitmap.CreateBitmap(bitmapLength, bitmapLength, Bitmap.Config.Argb8888);
+            canv = new Canvas(bmp);
+
+            CommonPageOutput(stopwatchLayout);
+
+            UpdateClock();
+            ButtonsState state;
+            if (timer == null)
+                state = ButtonsState.End;
+            else if (!timer.Enabled)
+                state = ButtonsState.Pause;
+            else
+                state = ButtonsState.Start;
+
+            ToggleButtons(state);
+        }
+
+        private void ListLayoutOutput()
+        {
+            CommonPageOutput(listLayout);
+
+            if (outputContainer.ChildCount == 0)
+                Toast.MakeText(this, "There is no registered time for the moment.", ToastLength.Long).Show();
+        }
+
+        private void GrachicsLayoutOutput()
+        {
+            CommonPageOutput(graphicsLayout);
+        }
+
+        private void SettingsLayoutOutput()
+        {
+            CommonPageOutput(settingsLayout);
+        }
+
+        private void SetButtonsListeners(Button startButton, Button stopButton, Button pauseButton, Button resetButton)
         {
             resetButton.Click += delegate
             {
@@ -250,7 +304,7 @@ namespace YourStopWatch
 
             startButton.Click += delegate
             {
-                ToggleButtonsStartTimer();
+                ToggleButtons(ButtonsState.Start);
                 UpdateClock();
                 absoluteRef = DateTime.Now;
                 timer = new Timer(10);
@@ -263,13 +317,13 @@ namespace YourStopWatch
             {
                 if (timer.Enabled)
                 {
-                    ToggleButtonsPauseTimer();
+                    ToggleButtons(ButtonsState.Start);
                     timer.Enabled = false;
                     pauseButton.Text = "continue";
                 }
                 else if (!timer.Enabled)
                 {
-                    ToggleButtonsStartTimer();
+                    ToggleButtons(ButtonsState.Start);
                     timer.Enabled = true;
                     pauseButton.Text = "pause";
                 }
@@ -277,7 +331,7 @@ namespace YourStopWatch
 
             stopButton.Click += delegate
             {
-                ToggleButtonsEndTimer();
+                ToggleButtons(ButtonsState.End);
                 Time savedTime = new Time
                 {
                     TimeSaved = ExtractTimerSpan()
@@ -286,8 +340,26 @@ namespace YourStopWatch
                 var db = new SQLiteConnection(System.IO.Path.Combine(dbFolder, dbName));
                 db.CreateTable<Time>();
                 db.Insert(savedTime);
-                db.Close();
+                AddTimeToOutputList(savedTime, db);
                 UpdateClock();
+            };
+        }
+
+        private void AddTimeToOutputList(Time t, SQLiteConnection db)
+        {
+            LayoutInflater inflater = (LayoutInflater)BaseContext.GetSystemService(Context.LayoutInflaterService);
+            View timeView = inflater.Inflate(Resource.Layout.single_output_layout, null);
+            TextView textOutput = timeView.FindViewById<TextView>(Resource.Id.outText);
+            Button removeButton = timeView.FindViewById<Button>(Resource.Id.removeButton);
+            outputContainer.AddView(timeView);
+
+            textOutput.Text = string.Format("{0}\n{1}", t.TimeSaved.ToLongDateString(), t.TimeSaved.ToLongTimeString());
+
+            removeButton.Click += delegate
+            {
+                outputContainer.RemoveView(timeView);
+                db.Delete<Time>(t.Id);
+                Toast.MakeText(this, $"The {t.TimeSaved.ToLongTimeString()} of {t.TimeSaved.ToShortDateString()} has been deleted.", ToastLength.Long).Show();
             };
         }
 
@@ -305,36 +377,43 @@ namespace YourStopWatch
             return time;
         }
 
-        private void ToggleButtonsStartTimer()
+        private void ToggleButtons(ButtonsState state)
         {
-            startButton.Enabled = false;
-            pauseButton.Enabled = true;
-            stopButton.Enabled = true;
-        }
+            Button startButton = stopwatchLayout.FindViewById<Button>(Resource.Id.startButton);
+            Button stopButton = stopwatchLayout.FindViewById<Button>(Resource.Id.stopButton);
+            Button pauseButton = stopwatchLayout.FindViewById<Button>(Resource.Id.pauseButton);
+            Button resetButton = stopwatchLayout.FindViewById<Button>(Resource.Id.resetButton);
 
-        private void ToggleButtonsPauseTimer()
-        {
-            startButton.Enabled = false;
-            pauseButton.Enabled = true;
-            stopButton.Enabled = false;
-        }
+            if (state == ButtonsState.Start)
+            {
+                startButton.Enabled = false;
+                pauseButton.Enabled = true;
+                stopButton.Enabled = true;
+            }
+            else if (state == ButtonsState.Pause)
+            {
+                startButton.Enabled = false;
+                pauseButton.Enabled = true;
 
-        private void ToggleButtonsEndTimer()
-        {
-            startButton.Enabled = true;
-            pauseButton.Enabled = false;
-            stopButton.Enabled = false;
+            }
+            else if (state == ButtonsState.End)
+            {
+                startButton.Enabled = true;
+                pauseButton.Enabled = false;
+                stopButton.Enabled = false;
+            }
         }
 
         public void UpdateClock()
         {
             timerView.Text = $"{hour}:{min}:{sec}:{milli/10}";
             canv.DrawColor(Color.Transparent, PorterDuff.Mode.Clear);
-            canv.DrawArc(hourOffset, hourOffset, bitmapLength - hourOffset, bitmapLength - hourOffset, -90, (hour + min / 60f) * 360f / maxHour, true, hourPaint);
+            canv.DrawArc(hourOffset, hourOffset, bitmapLength - hourOffset, bitmapLength - hourOffset, -90, (hour + min / 60f + sec / 3600f) * 360f / maxHour, true, hourPaint);
             canv.DrawArc(minOffset, minOffset, bitmapLength - minOffset, bitmapLength - minOffset, -90, (min + sec / 60f) * 360f / maxMin, true, minPaint);
             canv.DrawArc(secOffset, secOffset, bitmapLength - secOffset, bitmapLength - secOffset, -90, (sec + milli / 1000f) * 360f / maxSec, true, secPaint);
-            canv.DrawCircle(bitmapLength / 2f, bitmapLength / 2f, bitmapLength / 2f, contour);
-            imgView.SetImageBitmap(bmp);
+            if (showCircle)
+                canv.DrawCircle(bitmapLength / 2f, bitmapLength / 2f, bitmapLength / 2f, contour);
+            stopwatchImgView.SetImageBitmap(bmp);
         }
 
         private void Timer_Elapsed(object sender, ElapsedEventArgs e)
@@ -372,18 +451,27 @@ namespace YourStopWatch
             switch (item.ItemId)
             {
                 case Resource.Id.navigation_stopwatch:
-                    StopWatchPageOutput();
+                    StopWatchLayoutOutput();
                     return true;
                 case Resource.Id.navigation_list:
-                    ListPageOutput();
+                    ListLayoutOutput();
                     return true;
                 case Resource.Id.navigation_graphics:
+                    GrachicsLayoutOutput();
                     return true;
                 case Resource.Id.navigation_settings:
+                    SettingsLayoutOutput();
                     return true;
             }
             return false;
         }
+    }
+
+    public enum ButtonsState
+    {
+        Start,
+        End,
+        Pause
     }
 
     [Table("Times")]
